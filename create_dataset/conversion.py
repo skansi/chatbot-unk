d0b07670f3c13cd26f2a8ff7f897d2f144960b27
luka <prometej@prometej.in2data.eu>
2017-07-28 14:55:05 +0200

    Preprocessing text file

--- create_dataset/conversion.py
+++ create_dataset/conversion.py
@@ -1,57 +1,105 @@
 import re
 
-SOURCE = '/home/prometej/Workspaces/PythonWorkspace/chatbot-unk/Resources/Finance.txt'
+def conversion(SOURCE):
 
-# remove all titles
-with open(SOURCE, 'r+') as myfile:
-    data_whole = myfile.read()
+    # SOURCE = '/home/prometej/Workspaces/PythonWorkspace/chatbot-unk/Resources/enwiki-20170720-pages-articles-multistream.xml'
+    #
+    # DEST = '/home/prometej/Workspaces/PythonWorkspace/chatbot-unk/Resources/wikidump.txt'
 
-p = re.compile('=+\s([a-zA-Z\x7f-\xff]+\s)*=+')
-data = p.sub('', data_whole)
+    # remove all titles
+    with open(SOURCE, 'r+') as myfile:
+        data_whole = myfile.read()
 
-# remove all square brackets and everything inside them
-p = re.compile('\[[a-zA-Z\x7f-\xff]*[0-9]*\]')
-data = p.sub('', data_whole)
+    p = re.compile('=+\s([a-zA-Z\x7f-\xff]+\s)*=+')
+    data = p.sub('', data_whole)
 
-# remove all life span sequences
-p = re.compile('\([0-9]{4}.?[0-9]+\)')
-data = p.sub('', data_whole)
+    # remove all lines that start with <
+    p = re.compile('\<.*?\n')
+    data = p.sub('', data)
 
-# remove all years
-p = re.compile('[0-9]+')
-data = p.sub('', data_whole)
+    # remove all lines that start with *
+    p = re.compile('\*.*')
+    data = p.sub('', data)
 
-# remove all brackets and everything inside them
-p = re.compile('\((.*?)\)')
-data = p.sub('', data_whole)
+    # remove all square brackets and everything inside them
+    p = re.compile('\[[a-zA-Z\x7f-\xff]*[0-9]*\]')
+    data = p.sub('', data)
 
-# remove all commas
-p = re.compile(',')
-data = p.sub('', data_whole)
+    # remove all life span sequences
+    p = re.compile('\([0-9]{4}.?[0-9]+\)')
+    data = p.sub('', data)
 
-# remove replace all abbreviations
-p = re.compile('\s[A-Z][a-z]\.')
-data = p.sub('', data_whole)
+    # remove all years
+    p = re.compile('[0-9]+')
+    data = p.sub('', data)
 
-# remove replace all abbreviations -> capital letters
-p = re.compile('\s[A-Z][A-Z]\.')
-data = p.sub(' ', data_whole)
+    # remove all brackets and everything inside them
+    p = re.compile('\((.*?)\)')
+    data = p.sub('', data)
 
-# remove check if after any dot there is no space, than add one
-p = re.compile('\.')
-data = p.sub('. ', data_whole)
+    # remove all brackets
+    p = re.compile('\(')
+    data = p.sub('', data)
 
-# remove replace all dots with END_OF_SENTENCE symbol
-p = re.compile('\.\s')
-data = p.sub('$# ', data_whole)
+    # remove all brackets
+    p = re.compile('\)')
+    data = p.sub('', data)
 
-# remove replace two spaces with one
-p = re.compile('\s\s')
-data = p.sub(' ', data_whole)
+    # remove all commas
+    p = re.compile(',')
+    data = p.sub('', data)
 
-# replace new_line with single space
-p = re.compile('\n')
-data = p.sub(' ', data_whole)
+    # remove all commas
+    p = re.compile(':')
+    data = p.sub('', data)
 
-file = open(SOURCE, 'w+')
-file.write(data)
+    # remove all commas
+    p = re.compile(';')
+    data = p.sub('', data)
+
+    # remove all one letter abbreviations
+    p = re.compile('\s[A-z]\.')
+    data = p.sub('', data)
+
+    # remove all abbreviations
+    p = re.compile('\s[A-z][A-z]\.')
+    data = p.sub('', data)
+
+    # remove all " characters
+    p = re.compile('\"')
+    r = re.compile('\'')
+    data = p.sub('', data)
+    data = r.sub('', data)
+
+    # remove ... sequences
+    p = re.compile('\.{3}')
+    data = p.sub('', data)
+
+    # check if after any dot there is no space, than add one
+    p = re.compile('\.')
+    data = p.sub('. ', data)
+
+    # replace all dots with END_OF_SENTENCE symbol
+    p = re.compile('\.\s')
+    data = p.sub(' $# ', data)
+
+    # replace two spaces with one
+    p = re.compile('\s\s')
+    data = p.sub(' ', data)
+
+    # # add space before END_OF_SENTENCE symbol
+    # p = re.compile('\$\#')
+    # data = p.sub(' $#', data)
+
+    # replace new_line with single space
+    p = re.compile('\n')
+    data = p.sub(' ', data)
+
+    # replace two spaces with one
+    p = re.compile('\s\s')
+    data = p.sub(' ', data)
+
+    file = open(SOURCE, 'w+')
+    file.write(data)
+
+    return
