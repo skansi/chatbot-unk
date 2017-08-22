7744e2aeac20dccba49884a276ec0247452dd85d
Luka Novak <novak.luka93@gmail.com>
2017-08-22 14:48:10 +0200

    Results and files used for training

---
+++ word_based_version/vocab_formatter.py
@@ -0,0 +1,24 @@
+import os
+
+ROOTDIR = '/home/prometej/Workspaces/PythonWorkspace/Resources/wikidump/'
+
+with open('/home/prometej/Workspaces/PythonWorkspace/chatbot-unk/word_based_version/vocab2', 'r') as v:
+    VOCAB = eval(v.read())
+
+for subdir, dirs, files in os.walk(ROOTDIR):
+    print(subdir)
+    name = str(subdir).split('/')[-1]
+
+    for f in files:
+        print('Working on file:', f)
+        SOURCE = str(subdir) + '/' + str(f)
+        with open(SOURCE, 'r') as d:
+            data = d.read()
+            data = data.split()
+
+            for k in range(len(data)):
+                data[k] = ''.join([i for i in data[k] if i in VOCAB])
+
+            data = ' '.join(data)
+        with open(SOURCE, 'w+') as s:
+            s.write(data)
