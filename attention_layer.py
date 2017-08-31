c1a6049c10398dd852f8cb85c84caa01883ac3af
Antonio Šajatović <antonio.sajatovic@gmail.com>
2017-08-31 15:57:43 +0200

    Soft attention language model example

--- attention_layer.py
+++ attention_layer.py
@@ -5,84 +5,49 @@ from keras.layers.recurrent import *
 from keras.layers.embeddings import *
 from keras.layers.wrappers import *
 from keras.layers.core import *
-from keras.layers.convolutional import Conv1D
-from keras.layers.pooling import MaxPooling1D
 
 class Attention(Dense):
     """
-        Attention operation, with an optional context/query vector, for temporal data.
-        # Arguments
-            use_context: Boolean. Whether to use an optional context vector
-            return_sequences: Boolean. Whether to return the contexts only,
-             or the full sequence with concatenated matching contexts.
-            merge_mode: Mode by which contexts and timesteps will be combined.
-            One of {'sum', 'concat'}.
-        # Input shape
-            3D tensor with shape: `(batch_size, timesteps, input_dim)`.
-        # Output shape
-            - if `return_sequences` and 'concat': 3D tensor with shape `(batch_size, timesteps, 2*input_dim)`.
-            - else if `return_sequences` and 'sum': 3D tensor with shape `(batch_size, timesteps, input_dim)`.
-            - else, 2D tensor with shape `(batch_size, input_dim)`.
-        :param kwargs:
-        Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True,
-        and units=output_dim from the previous RNN layer.
-        Example:
-            model.add(LSTM(64, return_sequences=True))
-            model.add(Attention(64, activation='tanh'))
+    Keras Layer that implements an Attention mechanism,
+    with an optional context/query vector,for temporal data.
+    Supports Masking.
+    Follows the work of Baziotis et al. [aclweb.org/anthology/S17-2126]
+    # Input shape
+        3D tensor with shape `(batch_size, timesteps, features)`
+    # Output shape
+        2D tensor with shape `(batch_size, features)`
+    :param kwargs:
+    Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
+    The dimensions are inferred based on the output shape of the RNN.
+    Example:
+        model.add(GRU(100, return_sequences=True))
+        model.add(Attention(activation='tanh'))
     """
-    def __init__(self, use_context=True, return_sequences=True, merge_mode='concat', **kwargs):
+    def __init__(self, use_context=False, **kwargs):
         self.use_context = use_context
-        self.return_sequences = return_sequences
-        self.merge_mode = merge_mode
-        super(Attention, self).__init__(**kwargs)
+        super(Attention, self).__init__(units=1, **kwargs)
 
     def build(self, input_shape):
         assert len(input_shape) == 3
-        assert input_shape[-1] == self.units
-        # Create a trainable weight variable for this layer.
+        self.units = input_shape[-1]
         if self.use_context:
             self.context = self.add_weight(name='context', 
-                                      shape=(self.units,1),
+                                      shape=(self.units,),
                                       initializer='glorot_uniform',
                                       trainable=True)
+        else:
+            self.context = None
         super(Attention, self).build(input_shape)
 
     def call(self, inputs):
         outputs = super(Attention, self).call(inputs)
-        # Implements a soft attention mechanism
         if self.use_context:
-            outputs = K.dot(outputs, self.context)
+            outputs = K.squeeze(K.dot(outputs, K.expand_dims(self.context)), axis=-1)
         weights = K.softmax(outputs)
-        contexts = K.sum(weights*inputs, axis=1)
-
-        if self.return_sequences:
-            # Returns matching context appended to each timestep (i.e. word) in a sequence
-            input_shape = K.int_shape(inputs)
-            contexts = K.repeat_elements(contexts, input_shape[1], axis=0)
-            contexts = K.reshape(contexts, input_shape)
-            if self.merge_mode == 'concat':
-                output = K.concatenate([contexts, inputs])
-            elif self.merge_mode == 'sum':
-                output = contexts + inputs
-        else:
-            # Returns contexts for each input sequence
-            output = contexts
-
-        return output
+        return K.sum(inputs*weights, axis=1)
 
     def compute_output_shape(self, input_shape):
-        assert len(input_shape) == 3
-        batch_size, timesteps, input_dim = input_shape
-
-        if self.return_sequences:
-            if self.merge_mode == 'concat':
-                output_shape = (batch_size, timesteps, 2*input_dim)
-            elif self.merge_mode == 'sum':
-                output_shape = (batch_size, timesteps, input_dim)
-        else:
-            output_shape = (batch_size, input_dim)
-
-        return output_shape
+        return (input_shape[0], input_shape[-1]) 
 
 HIDDEN_SIZE = 300
 BATCH_SIZE = 32
@@ -96,17 +61,14 @@ consists of a 2-layer bidirectional GRU
 with an attention mechanism for identifying
 the most informative words.
 """
-print('Build model...')
 model = Sequential()
-#model.add(Embedding(input_dim=VOCAB_SIZE, output_dim=HIDDEN_SIZE, batch_size=BATCH_SIZE, input_shape=INPUT_SHAPE))
-model.add(Bidirectional(layer=GRU(HIDDEN_SIZE, return_sequences=True), merge_mode='concat', input_shape=INPUT_SHAPE, batch_size=BATCH_SIZE))
+#model.add(Embedding(input_dim=VOCAB_SIZE, output_dim=HIDDEN_SIZE, input_shape=INPUT_SHAPE))
+model.add(Bidirectional(layer=GRU(HIDDEN_SIZE, return_sequences=True), merge_mode='concat', input_shape=INPUT_SHAPE))
 model.add(Dropout(0.3))
 model.add(Bidirectional(layer=GRU(HIDDEN_SIZE, return_sequences=True), merge_mode='concat'))
 model.add(Dropout(0.3))
-model.add(Attention(units=2*HIDDEN_SIZE, activation='tanh', return_sequences=False))
-#model.add(GRU(units=2*HIDDEN_SIZE))
+model.add(Attention(activation='tanh'))
 model.add(Dense(units=VOCAB_SIZE, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
-print("All checks passed")
