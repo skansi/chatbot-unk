a4c5480115b75dd5c5e9a99d5ed1a6bcccc7d19b
Antonio Šajatović <antonio.sajatovic@gmail.com>
2017-09-01 15:51:03 +0200

    Attention layer fixed

--- attention_layer.py
+++ attention_layer.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras import backend as K
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers.recurrent import *
 from keras.layers.embeddings import *
 from keras.layers.wrappers import *
@@ -18,18 +18,18 @@ class Attention(Dense):
         2D tensor with shape `(batch_size, features)`
     :param kwargs:
     Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
-    The dimensions are inferred based on the output shape of the RNN.
+    The number of units has to be equal to the output shape of the RNN.
     Example:
         model.add(GRU(100, return_sequences=True))
-        model.add(Attention(activation='tanh'))
+        model.add(Attention(units=100, activation='tanh'))
     """
     def __init__(self, use_context=False, **kwargs):
         self.use_context = use_context
-        super(Attention, self).__init__(units=1, **kwargs)
+        super(Attention, self).__init__(**kwargs)
 
     def build(self, input_shape):
         assert len(input_shape) == 3
-        self.units = input_shape[-1]
+        assert self.units == input_shape[-1]
         if self.use_context:
             self.context = self.add_weight(name='context', 
                                       shape=(self.units,),
@@ -67,8 +67,15 @@ model.add(Bidirectional(layer=GRU(HIDDEN_SIZE, return_sequences=True), merge_mod
 model.add(Dropout(0.3))
 model.add(Bidirectional(layer=GRU(HIDDEN_SIZE, return_sequences=True), merge_mode='concat'))
 model.add(Dropout(0.3))
-model.add(Attention(activation='tanh'))
+model.add(Attention(units=2*HIDDEN_SIZE, activation='tanh'))
 model.add(Dense(units=VOCAB_SIZE, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
+
+model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+del model  # deletes the existing model
+
+# returns a compiled model
+# identical to the previous one
+model = load_model('my_model.h5',{"Attention":Attention})
